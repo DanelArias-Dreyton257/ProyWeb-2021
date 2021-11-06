8a967ee87f5fcd19fff746bdd1c9ec92f8b7a99c
DanelArias-Dreyton257 <danelarias7@gmail.com>
2021-11-06 11:06:53 +0100

    Anyadidas las vistas v.0.1

--- mammamia/mammamiaApp/views.py
+++ mammamia/mammamiaApp/views.py
@@ -2,10 +2,57 @@ from django.shortcuts import render
 
 # Create your views here.
 
-#--------------------PRUEBA--------------------
+def portada(request):
+    #TODO
+    #lista = get_list_or_404(Pizza)
+    #pizza = get_object_or_404(Pizza, pk=id_pizza)
+    context = {
+       #Anyadir lo que se necesite
+    }
+    return render(request,'portada.html', context)
 
-from django.http import HttpResponse
-def index(request):
-    return HttpResponse("Hello, world!")
+def listaPizzas(request):
+    lista = get_list_or_404(Pizza)
 
-#--------------------PRUEBA--------------------
+    context = {
+        'lista_pizzas': lista,
+    }
+    return render(request,'listaPizzas.html', context)
+
+def detallePizza(request, id_pizza):
+    pizza = get_object_or_404(Pizza, pk=id_pizza)
+
+    context = {
+        'pizza' :pizza,
+    }
+    return render(request,'detallePizza.html', context)
+
+def listaMasas(request):
+    lista = get_list_or_404(Masa)
+    context = {
+        'lista_masas': lista,
+    }
+    return render(request,'listaMasas.html', context)
+
+def detalleMasa(request, id_masa):
+    pizza = get_object_or_404(Masa, pk=id_masa)
+
+    context = {
+        'masa' :masa,
+    }
+    return render(request,'detalleMasa.html', context)
+
+def listaIngredientes(request):
+    lista = get_list_or_404(Ingrediente)
+    context = {
+        'lista_ingredientes': lista,
+    }
+    return render(request,'listaIngredientes.html', context)
+
+def detalleIngrediente(request, id_ingrediente):
+    pizza = get_object_or_404(Ingrediente, pk=id_ingrediente)
+
+    context = {
+        'ingrediente' :ingrediente,
+    }
+    return render(request,'detalleIngrediente.html', context)
