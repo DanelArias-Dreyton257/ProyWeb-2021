37386926edef223913cde2bed1ac7cc682f9cb15
DanelArias-Dreyton257 <danelarias7@gmail.com>
2021-11-06 11:13:46 +0100

    min_value no vale como agrumento de DecimalField // Arreglado

--- mammamia/mammamiaApp/models.py
+++ mammamia/mammamiaApp/models.py
@@ -6,14 +6,14 @@ class Masa(models.Model):
     nombre = models.CharField(max_length=50)
     grosormm = models.IntegerField(min_value=1)
     tipoHarina = models.CharField(max_length=50)
-    supPrecio = models.DecimalField(decimal_places=2, default=0, min_value=0)
+    supPrecio = models.DecimalField(decimal_places=2, default=0)
 
     def __str__(self):
         return "{}".format(self.nombre)
 
 class Pizza(models.Model):
     nombre = models.CharField(max_length=50)
-    precio = models.DecimalField(decimal_places=2, default=0, min_value=0)
+    precio = models.DecimalField(decimal_places=2, default=0)
     masa = models.ForeignKey(Masa, on_delete=models.CASCADE)
 
     #Falta ingredientes -Mirar como hacer tablas n a m
@@ -24,7 +24,7 @@ class Pizza(models.Model):
 class Ingrediente(models.Model):
     nombre = models.CharField(max_length=50)
     origen = models.CharField(max_length=50)
-    kcal = models.DecimalField(decimal_places=2, default=0, min_value=0)
+    kcal = models.DecimalField(decimal_places=2, default=0)
 
     #Falta pizzas -Mirar como hacer tablas n a m
 
