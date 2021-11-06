a4a9f5e75b2f1e8b27e1f1ac9596ecf83eeadfc9
DanelArias-Dreyton257 <danelarias7@gmail.com>
2021-11-06 11:05:59 +0100

    Creacion de modelos Masa, Pizza y Ingrediente

--- mammamia/mammamiaApp/models.py
+++ mammamia/mammamiaApp/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+class Masa(models.Model):
+    nombre = models.CharField(max_length=50)
+    grosormm = models.IntegerField(min_value=1)
+    tipoHarina = models.CharField(max_length=50)
+    supPrecio = models.DecimalField(decimal_places=2, default=0, min_value=0)
+
+    def __str__(self):
+        return "{}".format(self.nombre)
+
+class Pizza(models.Model):
+    nombre = models.CharField(max_length=50)
+    precio = models.DecimalField(decimal_places=2, default=0, min_value=0)
+    masa = models.ForeignKey(Masa, on_delete=models.CASCADE)
+
+    #Falta ingredientes -Mirar como hacer tablas n a m
+
+    def __str__(self):
+        return "{}".format(self.nombre)
+
+class Ingrediente(models.Model):
+    nombre = models.CharField(max_length=50)
+    origen = models.CharField(max_length=50)
+    kcal = models.DecimalField(decimal_places=2, default=0, min_value=0)
+
+    #Falta pizzas -Mirar como hacer tablas n a m
+
+    def __str__(self):
+        return "{}".format(self.nombre)
