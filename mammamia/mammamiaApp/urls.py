15c7c34b6a71056b6eac292570d5ef2001a39394
DanelArias-Dreyton257 <danelarias7@gmail.com>
2021-11-06 11:08:35 +0100

    Actualizado urls.py para las vistas

--- mammamia/mammamiaApp/urls.py
+++ mammamia/mammamiaApp/urls.py
@@ -3,9 +3,21 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #mammamia/home
+    path('home', views.portada, name='portada'),
 
-#--------------------PRUEBA--------------------
-    path('', views.index, name="index"),
-#--------------------PRUEBA--------------------
+    #mammamia/pizzas
+    path('pizzas', views.listaPizzas, name='pizzas'),
+    #mammamia/pizzas/[id_pizza]
+    path('pizzas/<int:id_pizza>', views.detallePizza,name='detalle_pizza'),
 
+    #mammamia/masas
+    path('masas', views.listaMasas, name='masa'),
+    #mammamia/masas/[id_masa]
+    path('masas/<int:id_masa>', views.detalleMasa, name='detalle_masa'),
+
+    #mammamia/ingredientes
+    path('masas', views.listaIngredientes, name='ingredientes'),
+    #mammamia/masas/[id_ingrediente]
+    path('masas/<int:id_ingrediente>', views.detalleIngrediente, name='detalle_ingrediente'),
 ]
