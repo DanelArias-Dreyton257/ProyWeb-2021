b11790966574b6e20b8a53a1e8b062a353e62f99
Oier Mentxaka <oiermentxaka@gmail.com>
2021-11-30 09:33:11 +0100

    sustitucion de vistas basadas en funciones por vistas basadas en clases

--- mammamia/mammamiaApp/urls.py
+++ mammamia/mammamiaApp/urls.py
@@ -1,20 +1,20 @@
 from django.urls import path
 
-from . import views
+from .views import *
 
 urlpatterns = [
     #mammamia/home
-    path('home', views.portada, name='portada'),
+    path('home', Portada.as_view(), name='portada'),
     #mammamia/pizzas
-    path('pizzas', views.listaPizzas, name='pizzas'),
+    path('pizzas', ListaPizzas.as_view(), name='pizzas'),
     #mammamia/pizzas/[id_pizza]
-    path('pizzas/<int:id_pizza>', views.detallePizza,name='detalle_pizza'),
+    path('pizzas/<int:pk>', DetallePizza.as_view(),name='detalle_pizza'),
     #mammamia/masas
-    path('masas', views.listaMasas, name='masas'),
+    path('masas', ListaMasas.as_view(), name='masas'),
     #mammamia/masas/[id_masa]
-    path('masas/<int:id_masa>', views.detalleMasa, name='detalle_masa'),
+    path('masas/<int:pk>', DetalleMasa.as_view(), name='detalle_masa'),
     #mammamia/ingredientes
-    path('ingredientes', views.listaIngredientes, name='ingredientes'),
+    path('ingredientes', ListaIngredientes.as_view(), name='ingredientes'),
     #mammamia/masas/[id_ingrediente]
-    path('ingredientes/<int:id_ingrediente>', views.detalleIngrediente, name='detalle_ingrediente'),
+    path('ingredientes/<int:pk>', DetalleIngrediente.as_view(), name='detalle_ingrediente'),
 ]
--- mammamia/mammamiaApp/views.py
+++ mammamia/mammamiaApp/views.py
@@ -1,68 +1,127 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from .models import Pizza, Masa, Ingrediente
+from django.views.generic.list import *
+from django.views.generic.detail import *
 
-# Create your views here.
+
+# Vistas basadas en funciones
 
 # Esta funcion mostrara una portada, la cual, aparecera una lista de masas, con una pizza por cada masa
-def portada(request):
-    lista_m = get_list_or_404(Masa)
+# def portada(request):
+#     lista_m = get_list_or_404(Masa)
+# 
+#     lista_min_p = []
+#     for masa in lista_m: #Busca las pizzas mas baratas de cada masa
+#         lista_min_p.append(min(masa.pizza_set.all()))
+# 
+#     context = {
+#        'lista_min_p' : lista_min_p,
+#     }
+#     return render(request,'portada.html', context)
+# 
+# Esta funcion mostrara un listado de todas las pizzas
+# def listaPizzas(request):
+#     lista = get_list_or_404(Pizza.objects.all().order_by('nombre'))
+#     context = {
+#         'lista_pizzas': lista,
+#     }
+#     return render(request,'listaPizzas.html', context)
+# 
+# Esta funcion mostrara un listado de todas las masas
+# def listaMasas(request):
+#     lista = get_list_or_404(Masa.objects.all().order_by('nombre'))
+#     context = {
+#         'lista_masas': lista,
+#     }
+#     return render(request,'listaMasas.html', context)
+#
+# Esta funcion mostrara un listado de todos los ingredientes
+# def listaIngredientes(request):
+#     lista = get_list_or_404(Ingrediente.objects.all().order_by('nombre'))
+#     context = {
+#         'lista_ingredientes': lista,
+#     }
+#     return render(request,'listaIngredientes.html', context)
+# 
+# Esta funcion mostrara todos los detalles de la pizza
+# def detallePizza(request, id_pizza):
+#     pizza = get_object_or_404(Pizza, pk=id_pizza)
+# 
+#     context = {
+#         'pizza' :pizza,
+#     }
+#     return render(request,'detallePizza.html', context)
+# 
+# Esta funcion mostrara todos los detalles de la masa
+# def detalleMasa(request, id_masa):
+#     masa = get_object_or_404(Masa, pk=id_masa)
+# 
+#     context = {
+#         'masa' :masa,
+#     }
+#     return render(request,'detalleMasa.html', context)
+# 
+# Esta funcion mostrara todos los detalles de los ingredientes
+# def detalleIngrediente(request, id_ingrediente):
+#     ingrediente = get_object_or_404(Ingrediente, pk=id_ingrediente)
+# 
+#     context = {
+#         'ingrediente' :ingrediente,
+#     }
+#     return render(request,'detalleIngrediente.html', context)
+# 
+# Vistas basadas en clases
 
+# Esta funcion mostrara una portada, la cual, aparecera una lista de masas, con una pizza por cada masa
+class Portada(ListView):
+    model = Masa
     lista_min_p = []
+    lista_m = get_list_or_404(Masa)
     for masa in lista_m: #Busca las pizzas mas baratas de cada masa
         lista_min_p.append(min(masa.pizza_set.all()))
-
-    context = {
-       'lista_min_p' : lista_min_p,
-    }
-    return render(request,'portada.html', context)
+    queryset = lista_min_p
+    template_name = "portada.html"
+    context_object_name = "lista_min_p"
 
 # Esta funcion mostrara un listado de todas las pizzas
-def listaPizzas(request):
-    lista = get_list_or_404(Pizza.objects.all().order_by('nombre'))
-    context = {
-        'lista_pizzas': lista,
-    }
-    return render(request,'listaPizzas.html', context)
+class ListaPizzas(ListView):
+    model = Pizza
+    lista = get_list_or_404(Pizza)
+    queryset = lista
+    template_name = "listaPizzas.html"
+    context_object_name = "lista_pizzas"
 
 # Esta funcion mostrara un listado de todas las masas
-def listaMasas(request):
+class ListaMasas(ListView):
+    model = Masa
     lista = get_list_or_404(Masa.objects.all().order_by('nombre'))
-    context = {
-        'lista_masas': lista,
-    }
-    return render(request,'listaMasas.html', context)
+    queryset = lista
+    template_name = "listaMasas.html"
+    context_object_name = "lista_masas"
 
 # Esta funcion mostrara un listado de todos los ingredientes
-def listaIngredientes(request):
+class ListaIngredientes(ListView):
+    model = Ingrediente
     lista = get_list_or_404(Ingrediente.objects.all().order_by('nombre'))
-    context = {
-        'lista_ingredientes': lista,
-    }
-    return render(request,'listaIngredientes.html', context)
+    queryset = lista
+    template_name = "listaIngredientes.html"
+    context_object_name = "lista_ingredientes"
 
 # Esta funcion mostrara todos los detalles de la pizza
-def detallePizza(request, id_pizza):
-    pizza = get_object_or_404(Pizza, pk=id_pizza)
+class DetallePizza(DetailView):  
+    model = Pizza
+    template_name = "detallePizza.html"
+    context_object_name = "pizza"
 
-    context = {
-        'pizza' :pizza,
-    }
-    return render(request,'detallePizza.html', context)
 
 # Esta funcion mostrara todos los detalles de la masa
-def detalleMasa(request, id_masa):
-    masa = get_object_or_404(Masa, pk=id_masa)
-
-    context = {
-        'masa' :masa,
-    }
-    return render(request,'detalleMasa.html', context)
+class DetalleMasa(DetailView):  
+    model = Masa
+    template_name = "detalleMasa.html"
+    context_object_name = "masa"
 
 # Esta funcion mostrara todos los detalles de los ingredientes
-def detalleIngrediente(request, id_ingrediente):
-    ingrediente = get_object_or_404(Ingrediente, pk=id_ingrediente)
-
-    context = {
-        'ingrediente' :ingrediente,
-    }
-    return render(request,'detalleIngrediente.html', context)
+class DetalleIngrediente(DetailView):  
+    model = Ingrediente
+    template_name = "detalleIngrediente.html"
+    context_object_name = "ingrediente"
